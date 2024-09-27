d00d56af05aa16008a7c4f724ca7d700ce0f296c
Brayan Ramos <82058263+brayan-cincoe@users.noreply.github.com>
2024-09-26 21:54:24 -0500

    feat: add bottom navigaton bar

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:cinemapp/presentation/widgets/movies/movies_slideshow.dart';
 export 'package:cinemapp/presentation/widgets/shared/custom_appbar.dart';
+export 'package:cinemapp/presentation/widgets/shared/custom_bottom_navbar.dart';

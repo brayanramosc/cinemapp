c35b715a070849e3d87afd0839a0f022421f0a80
Brayan Ramos <82058263+brayan-cincoe@users.noreply.github.com>
2024-10-06 18:31:18 -0500

    feat: add movie info provider to get selected movie

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatefulWidget {
+import 'package:cinemapp/domain/entity/movie.dart';
+import 'package:cinemapp/presentation/providers/movies/movie_info_provider.dart';
 
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie-screen';
   final String movieId;
 
   const MovieScreen({super.key, required this.movieId});
 
   @override
-  State<MovieScreen> createState() => _MovieScreenState();
+  MovieScreenState createState() => MovieScreenState();
 }
 
-class _MovieScreenState extends State<MovieScreen> {
+class MovieScreenState extends ConsumerState<MovieScreen> {
   @override
   void initState() {
     super.initState();
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
   }
 
   @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+
+    if (movie == null) {
+      return const Scaffold(
+          body: Center(child: CircularProgressIndicator(strokeWidth: 2)));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('MovieID: ${widget.movieId}'),
